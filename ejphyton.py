5c1b7812e55796de651280f028dff72838a11f45
mateogonzalezdw <mateogaelgonzalezbarila@gmail.com>
2025-04-07 16:15:02 -0300

    Ejercicio1

---
+++ ejphyton.py
@@ -0,0 +1,6 @@
+def EJ1():
+    numero1 = int(input("Ingrese el primer número: "))
+    numero2 = int(input("Ingrese el segundo número: "))
+    print("La suma de", numero1, "más", numero2, "es igual a", (numero1 + numero2))
+
+#EJ1()
